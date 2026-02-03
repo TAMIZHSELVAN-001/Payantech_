e9ff363929a438d6a3d5dffa614372349c9be1ea
TAMIZHSELVAN-001 <tamizhselvan2705@gmail.com>
2026-02-03 15:29:29 +0530

    Update settings.py

--- payantech/settings.py
+++ payantech/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#f+i*9qk^*qvk-9@t5z&6=ke3gi%kt+dtqe)td2u#pm2i8=(9'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
