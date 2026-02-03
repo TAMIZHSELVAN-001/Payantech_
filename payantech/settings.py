121379faf48e2ef538f41d6e08efd340ecb39885
TAMIZHSELVAN-001 <tamizhselvan2705@gmail.com>
2026-02-03 16:40:02 +0530

    Update settings.py

--- payantech/settings.py
+++ payantech/settings.py
@@ -132,12 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "main" / "static"
 ]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files configuration
 MEDIA_URL = '/media/'
