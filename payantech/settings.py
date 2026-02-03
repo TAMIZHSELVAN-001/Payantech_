b6b03468eab683cb22e42dc31ed4404a09832177
TAMIZHSELVAN-001 <tamizhselvan2705@gmail.com>
2026-02-03 14:00:50 +0530

    Update settings.py

--- payantech/settings.py
+++ payantech/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-9#f+i*9qk^*qvk-9@t5z&6=ke3gi%kt+dtqe)td2u#pm2i8=(9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "main" / "static"
 ]
